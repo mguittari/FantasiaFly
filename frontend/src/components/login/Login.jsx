baca23cc34bdfb67b338091fa83c63277525e852
Meriem Chikha <meriem.chikha@laposte.net>
2024-04-09 16:14:01 +0200

    C:/Program Files/Git/me ok

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
 
 export default function Login() {
-  return <Link to="/connexion" />;
+  const { user } = useContext(UserContext);
+  console.info("user:>>", user);
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {user.message === "isLogged" ? (
+        <p>{user.user.lastname}</p>
+      ) : (
+        <ul className="hidden md:flex gap-8 ">
+          <li>
+            <Link to="/connexion"> Connexion</Link>
+          </li>
+          <li>
+            <Link to="/">Inscription</Link>
+          </li>
+        </ul>
+      )}
+    </>
+  );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Layout from "./components/layout/Layout";
 import LoginPage from "./pages/login-page/LoginPage";
+import { UserProvider } from "./context/userContext";
 
 function AppLayout() {
   return (
@@ -29,6 +30,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
